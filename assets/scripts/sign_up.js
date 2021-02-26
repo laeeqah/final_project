b5ccaa5deb9145dbd67c4adb42c6a1ae0acf0a85
laeeqah <laeeqahesau@gmail.com>
2021-02-26 14:22:11 +0200

    new products

--- assets/scripts/sign_up.js
+++ assets/scripts/sign_up.js
@@ -1,1 +1,21 @@
-fetch("https://arcane-shelf-35923.herokuapp.com/");
+function createUsers() {
+  const inputs = document.getElementsByTagName("input");
+
+  fetch("https://arcane-shelf-35923.herokuapp.com/main/", {
+    method: "POST",
+    body: JSON.stringify({
+      fullname: inputs[0].value,
+      username: inputs[1].value,
+      email: inputs[2].value,
+      password: inputs[3].value,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      alert("user has been created");
+      document.getElementById("signup-form").reset();
+    });
+}
