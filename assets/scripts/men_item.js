ef0a461fc4482d1da512f920f074f43a388ab461
laeeqah <laeeqahesau@gmail.com>
2021-03-09 13:21:22 +0200

    up-to-date

--- assets/scripts/men_item.js
+++ assets/scripts/men_item.js
@@ -1,6 +1,16 @@
 let slideIndex = 0;
 showSlides();
 
+function PopUpShow() {
+  let popup = document.getElementById("product-popup");
+  popup.style.display = "block";
+}
+
+function PopUpClose() {
+  popup = document.getElementById("product-popup");
+  popup.style.display = "none";
+}
+
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -29,16 +39,80 @@ function productListed() {
       console.log(list);
       data.forEach((product) => {
         let item = ` 
-              <div class ="card">
-                <h4>${product.catergories}</h4>
+            
+            <div class ="card">
+            <h4>${product.categories}</h4>
+              <div class="product-image">
                 <img src=${product.images} />
+              </div>
+              <div class="product-info">
                 <h4>${product.name}</h4>
                 <h4>${product.price}</h4>
                 <button class = "cart">Add Cart</button>
-              </div>`;
+                <button class="popup-btn" onClick="PopUpShow()" id="viewBtn">View Product</button>
+                <div id="product-popup" class="popup">
+                    <div class="popup-content">
+                        <div class="popup-title text-center">
+                            <span onClick ="PopUpClose()" class="close">&times;</span>
+                            <h2>${product.name}</h2>
+                            <img src=${product.images} />
+                        </div>
+                        <div class ="popup-price">
+                            <h3>Price: ${product.price}</h3>
+                        </div>
+                        <div class="popup-info text-left">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, maiores.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium labore atque ducimus nulla eius eos
+                                illo quae vero facere. Atque!.</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, maiores.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium labore atque ducimus nulla eius eos
+                                illo quae vero facere. Atque!.</p>
+                        </div>
+                        <div class = "popup-cate>
+                          <h4>Catergory: ${product.categories}</h4>
+                        </div>
+                        <div class = "popup-size">
+                          <h3>Size: ${product.size}</h3>
+                        </div>
+
+                    </div>
+                </div>
+              </div>
+                
+            </div>`;
+
         console.log(item);
         list.innerHTML += item;
       });
     });
 }
+
 productListed();
+// PopUpClose();
+// PopUpShow();
+
+let btn = document.getElementById("viewBtn");
+
+button.onclick = function (event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+};
+
+// SEARCH BAR
+function searchProduct() {
+  const searchForm = document.getElementById("search-form");
+  const search = searchForm.getElementsByTagName("input")[0].value;
+
+  fetch("https://arcane-shelf-35923.herokuapp.com/list-men/")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      console.log(search);
+      let matchedSearch = json.filter((data) => {
+        return data.title == search;
+      });
+      document.getElementById("products").innerHTML = "";
+      matchedSearch.forEach((product) => createProductItem(product));
+    });
+}
--- assets/scripts/women_item.js
+++ assets/scripts/women_item.js
@@ -1,6 +1,16 @@
 let slideIndex = 0;
 showSlides();
 
+function PopUpShow() {
+  let popup = document.getElementById("product-popup");
+  popup.style.display = "block";
+}
+
+function PopUpClose() {
+  popup = document.getElementById("product-popup");
+  popup.style.display = "none";
+}
+
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -39,16 +49,33 @@ function productListed() {
                 <h4>${product.name}</h4>
                 <h4>${product.price}</h4>
                 <button class = "cart">Add Cart</button>
-                <button id = "viewBtn">View Item</button>
-                <!-- The Modal -->
-                <div id="myModal" class="modal">
+                <button class="popup-btn" onClick="PopUpShow()" id="viewBtn">View Product</button>
+                <div id="product-popup" class="popup">
+                    <div class="popup-content">
+                        <div class="popup-title text-center">
+                            <span onClick ="PopUpClose()" class="close">&times;</span>
+                            <h2>${product.name}</h2>
+                            <img src=${product.images} />
+                        </div>
+                        <div class ="popup-price">
+                            <h3>Price: ${product.price}</h3>
+                        </div>
+                        <div class="popup-info text-left">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, maiores.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium labore atque ducimus nulla eius eos
+                                illo quae vero facere. Atque!.</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, maiores.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium labore atque ducimus nulla eius eos
+                                illo quae vero facere. Atque!.</p>
+                        </div>
+                        <div class = "popup-cate>
+                          <h4>Catergory: ${product.categories}</h4>
+                        </div>
+                        <div class = "popup-size">
+                          <h3>Size: ${product.size}</h3>
+                        </div>
 
-                  <!-- Modal content -->
-                  <div class="modal-content">
-                    <span class="close">&times;</span>
-                    <p>Some text in the Modal..</p>
-                  </div>
-
+                    </div>
                 </div>
               </div>
                 
@@ -61,29 +88,32 @@ function productListed() {
 }
 
 productListed();
+// PopUpClose();
+// PopUpShow();
 
-// MODAL
-let modal = document.getElementById("myModal");
-
-// THE BUTTON THAT OPENS THE MODAL
 let btn = document.getElementById("viewBtn");
 
-// THE <span> ELEMENT THAT CLOSES THE MODEL
-let span = document.getElementsByClassName("close")[0];
-
-// CLICK ON THE BUTTON MODEL OPENS
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-// USER CLICK ON <span> (x) IT WILL CLOSE THE MODEL
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// ANYWHERE CLICKED OUTSIDE THE MODAL IT WILL CLOSE
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+button.onclick = function (event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
   }
 };
+
+function searchProduct() {
+  const searchForm = document.getElementById("search-form");
+  const search = searchForm.getElementsByTagName("input")[0].value;
+
+  fetch("https://arcane-shelf-35923.herokuapp.com/list-women/")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      console.log(search);
+      let matchedSearch = json.filter((data) => {
+        return data.title == search;
+      });
+      document.getElementById("products").innerHTML = "";
+      matchedSearch.forEach((product) => createProductItem(product));
+    });
+}
+
+//fetch the data
